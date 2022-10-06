2ef9e6c164996e6d8becf292d7d500b00168b38c
GuiMar01 <81651234+GuiMar01@users.noreply.github.com>
2022-10-06 16:05:38 +0100

    Update serialization.c
    serialization

--- source/serialization.c
+++ source/serialization.c
@@ -4,11 +4,44 @@
  * da função tree_get_keys. Para além disso, retorna o tamanho do
  * buffer alocado ou -1 em caso de erro.
  */
-int keyArray_to_buffer(char **keys, char **keys_buf) {}
+int keyArray_to_buffer(char **keys, char **keys_buf) {
+
+    if (**keys == NULL || *keys_buf == NULL) {
+        return -1;
+    }
+
+    int size = htonl (sizeof (keys));
+    int buffersize = sizeof (int) + sizeof (keys) + 1;
+    *keys_buf = malloc (buffersize);
+
+    if (*keys_buf == NULL) {
+        return -1;
+    }
+
+    memcpy (keys_buf, &size, sizeof (int));
+    memcpy (keys_buf + sizeof (int), &keys, buffersize);
+
+    return buffersize;
+}
 
 /* De-serializa a mensagem contida em keys_buf, com tamanho
  * keys_buf_size, colocando-a e retornando-a num array char**,
  * cujo espaco em memória deve ser reservado. Devolve NULL
  * em caso de erro.
  */
-char **buffer_to_keyArray(char *keys_buf, int keys_buf_size);
+char **buffer_to_keyArray(char *keys_buf, int keys_buf_size) {
+
+    if (keys_buf == NULL || keys_buf_size < 0) {
+        return NULL;
+    }
+
+    char **returnarray = malloc (keys_buf_size);
+
+    if (returnarray == NULL) {
+        return NULL;
+    }
+
+    memcpy (returnarray, &keys_buf + sizeof (int), keys_buf_size - sizeof (int));
+
+    return returnarray;
+}
